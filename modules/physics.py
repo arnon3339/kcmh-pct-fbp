ba6f875d55613cd0dfbde06cfdd35205db9b64a6
arnon <arnon3339@gmail.com>
2024-11-19 15:35:48 +0700

    fist

---
+++ modules/physics.py
@@ -0,0 +1,142 @@
+import os
+from os import path
+import uproot
+import pandas as pd
+import numpy as np
+import modules.plots as plot
+from modules import utils, analyse
+import warnings
+
+OUTDIR = "/data/pct/sim/cal/output"
+
+DENSITY_DICT = {
+    'alpide': 2.33,
+    'absorber': 2.70
+}
+
+WATER_DENS = 1
+ALUMINIUM_DENS = 2.7
+SILICON_DENS = 2.33
+AIR_DENS = 0.0012
+
+def get_thickness(current_pos, next_pos, ref=False):
+    abs_thickness = 3.5 #mm
+    alpide_thickness = 50e-3 #mm
+    dtc_distance = 25 #mm
+    alpide_size_x = 30 #mm
+    alpide_size_y = 15 #mm
+    num_alpide_x = 9
+    num_alpide_y = 12
+    num_pixels_x_in_alpide = 1024
+    num_pixels_y_in_alpide = 512
+    pixel_size_x = alpide_size_x/num_pixels_x_in_alpide
+    pixel_size_y = alpide_size_y/num_pixels_y_in_alpide
+
+    pos = np.array((
+        (next_pos[0] - current_pos[0])*pixel_size_x, 
+        (next_pos[1] - current_pos[1])*pixel_size_y, 
+        1*dtc_distance
+        ))
+    ref_pos = np.array((
+        alpide_size_x*num_alpide_x/2, 
+        alpide_size_y*num_alpide_y/2,
+        1*dtc_distance
+        ))
+    
+    dot_product = np.dot(pos, ref_pos)
+    norm_a = np.linalg.norm(pos)
+    norm_b = np.linalg.norm(ref_pos)
+
+    if (norm_a * norm_b) < dot_product:
+        angle = 0
+    else:
+        angle = np.arccos(dot_product / (norm_a * norm_b))
+
+    return {
+            'alpide': alpide_thickness/np.cos(angle), 
+            'absorber': abs_thickness/np.cos(angle)
+            }
+
+def get_WEPL(thick, en, mat='water'):
+    water_range = utils.get_range(en, 'water')
+    if mat.lower() == 'dtc':
+        al_range = utils.get_range(en, 'aluminium')
+        silicon_range = utils.get_range(en, 'silicon')
+        air_range = utils.get_range(en, 'air')
+
+        range_avg = 1./((1./silicon_range)*0.05 +
+            (1./al_range)*3.5 + (1./air_range)*(25. - 3.5 - 0.05))
+        
+        print(f'water range: {water_range}')
+        print(f'aluminium range: {al_range}')
+        print(f'silicon range: {silicon_range}')
+        print(f'air range: {air_range}')
+        print(f'average range: {range_avg}')
+        return thick*water_range/range_avg
+    else:
+        return thick*water_range/utils.get_range(en, mat)
+
+def get_proton_range(en, wepl=False):
+    en_dir_name = f'eMeV_{"0"*(3 - len(str(en))) + str(en)}' 
+    root_files = [
+        f for f in os.listdir(
+            path.join(
+                OUTDIR, 
+                en_dir_name,
+                'projection_000'
+                )
+            ) if '_t' in f
+        ]
+
+    num_proton = 0
+    thicknesses = []
+    for f in root_files:
+        with uproot.open(path.join(
+                OUTDIR,
+                en_dir_name, 
+                'projection_000',
+                f
+            )) as root_data:
+
+            tree = root_data['pCT;1']
+            df = tree.arrays(library='pd')
+
+            if len(df.index):
+                num_proton += len(df[df.layerID == 0].index)
+                df_unique_sum = df.groupby('eventID',
+                                        as_index=False)['thickness'].sum()
+                if wepl:
+                    thicknesses += (
+                        get_WEPL(df_unique_sum['thickness'].values, en, 'dtc')
+                        ).tolist()
+                else:
+                    thicknesses += df_unique_sum['thickness'].values.tolist()
+    
+    try:
+        data = np.array(thicknesses)
+        x_data = np.linspace(data.min(), data.max(), 200)
+        y_data = np.array([np.sum(data >= x) for x in x_data])
+        return x_data[np.where(y_data <= num_proton/2.)[0][0]]
+    except:
+        return -1
+    # if len(y_data):
+    #     return 
+    # plot.plot_dtc_range([x_data, y_data], 
+    #                     name="dtc_range_plot_wepl" if wepl else "dtc_range_plot")
+    # print(x_data[np.where(y_data <= num_proton/2.)[0][0]])
+
+def get_en_from_dtc(thick):
+    cofs = [-3.40743164e-05, 2.75444697e-02,1.31743588e-01, -9.22327146e+00 - thick]
+    try:
+        result = np.roots(cofs)
+        return result[(result > 0) & (result < 230)][0]
+    except:
+        return 0.
+
+def get_ren_from_dtc(en, thick):
+    cofs = [-3.40743164e-05, 2.75444697e-02,1.31743588e-01, -9.22327146e+00 - thick]
+    try:
+        result = np.roots(cofs)
+        return en - result[(result > 0) & (result < 230)][0]
+    except:
+        return en
---
+++ modules/plots.py
@@ -0,0 +1,77 @@
+import matplotlib.pyplot as plt
+
+def plot_dtc_hits_2d(*args, **kwargs):
+    if 'data' in kwargs.keys():
+        data = kwargs['data']
+    else:
+        data = args[0]
+    
+    x_min = kwargs['x_min'] if 'x_min' in kwargs.keys() else int(min(data[:, 0]))
+    x_max = kwargs['x_max'] if 'x_max' in kwargs.keys() else int(max(data[:, 0]))
+    y_min = kwargs['y_min'] if 'y_min' in kwargs.keys() else int(min(data[:, 1]))
+    y_max = kwargs['y_max'] if 'y_max' in kwargs.keys() else int(max(data[:, 1]))
+    xbin = kwargs['xbin'] if 'xbin' in kwargs.keys() else 1
+    ybin = kwargs['ybin'] if 'ybin' in kwargs.keys() else 1
+    fig, ax = plt.subplots(figsize=(12, 10))
+    ax.hist2d(data[:, 0], data[:, 1], 
+              bins=(range(x_min, x_max, xbin), range(y_min, y_max, ybin)))
+    plt.savefig(f"""./output/images/{kwargs["name"] 
+                if "name" in kwargs.keys() else "dtc_hits_2d.png"}""", dpi=300)
+    # plt.show()
+
+def plot_dtc_wepl_hist_1d(*args, **kwargs):
+    if 'data' in kwargs.keys():
+        data = kwargs['data']
+    else:
+        data = args[0]
+
+    x_min = kwargs['x_min'] if 'x_min' in kwargs.keys() else int(min(data))
+    x_max = kwargs['x_max'] if 'x_max' in kwargs.keys() else int(max(data))
+    xbin = kwargs['xbin'] if 'xbin' in kwargs.keys() else 1
+    fig, ax = plt.subplots(figsize=(12, 10))
+    plt.hist(data, bins=range(x_min, x_max, xbin))
+    plt.savefig(f"""./output/images/{kwargs["name"] if "name"
+        in kwargs.keys() else "dtc_wepl_hits_1d.png"}""", dpi=300)
+
+def plot_dtc_range(*args, **kwargs):
+    if 'data' in kwargs.keys():
+        data = kwargs['data']
+    else:
+        data = args[0]
+
+    fig, ax = plt.subplots(figsize=(12, 10))
+    ax.plot(data[0], data[1])
+    ax.set_xlim([0, 1.1*max(data[0])])
+    plt.savefig(f"""./output/images/{kwargs["name"] if "name"
+        in kwargs.keys() else "dtc_range.png"}""", dpi=300)
+
+def plot_entries_thickness_hist_1d(*args, **kwargs):
+    if 'data' in kwargs.keys():
+        data = kwargs['data']
+    else:
+        data = args[0]
+
+    x_min = kwargs['x_min'] if 'x_min' in kwargs.keys() else int(min(data))
+    x_max = kwargs['x_max'] if 'x_max' in kwargs.keys() else int(max(data))
+    xbin = kwargs['xbin'] if 'xbin' in kwargs.keys() else 1
+    fig, ax = plt.subplots(figsize=(12, 10))
+    plt.hist(data, bins=range(x_min, x_max, xbin))
+    plt.savefig(f"""./output/images/{kwargs["name"] if "name"
+        in kwargs.keys() else "entries_thickness_hits_1d.png"}""", dpi=300)
+
+def plot_range_en(*args, **kwargs):
+    aluminium_dens = 2.7
+    silicon_dens = 2.33
+    air_dens = 0.0012
+    avg_dens = (aluminium_dens*3.511 + silicon_dens*0.039 +
+        air_dens*(25. - 3.511 - 0.039))/(25.)
+    if 'data' in kwargs.keys():
+        data = kwargs['data']
+    else:
+        data = args[0]
+    
+    fig, ax = plt.subplots(figsize=(12, 10))
+    # ax.plot(data['energy'], data['range'])
+    ax.scatter(data['energy'], data['range']*avg_dens, marker='_')
+    plt.savefig(f"""./output/images/{kwargs["name"] if "name"
+        in kwargs.keys() else "proton_range_en.png"}""", dpi=300)
